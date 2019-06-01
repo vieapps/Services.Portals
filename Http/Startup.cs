b1f80e90517316cc8455b1b91744685d200ab73e
Quynh Nguyen <mr.quynh@gmail.com>
2019-06-02 01:37:24 +0700

    Improvement: data protection across load balancers

--- Http/Startup.cs
+++ Http/Startup.cs
@@ -53,7 +53,7 @@ namespace net.vieapps.Services.Portals
 				.AddSession(options =>
 				{
 					options.IdleTimeout = TimeSpan.FromMinutes(30);
-					options.Cookie.Name = "VIEApps-Session";
+					options.Cookie.Name = UtilityService.GetAppSetting("DataProtection:Name:SessionCookie", "VIEApps-Session");
 					options.Cookie.HttpOnly = true;
 				});
 
@@ -62,44 +62,54 @@ namespace net.vieapps.Services.Portals
 				.AddAuthentication(options => options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
 				.AddCookie(options =>
 				{
-					options.Cookie.Name = "VIEApps-Auth";
+					options.Cookie.Name = UtilityService.GetAppSetting("DataProtection:Name:AuthenticationCookie", "VIEApps-Auth");
 					options.Cookie.HttpOnly = true;
 					options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
 					options.SlidingExpiration = true;
 				});
 
-			// data protection
-			services
-				.AddDataProtection()
+			// data protection (encrypt/decrypt authenticate ticket cookies & sync across load balancers)
+			var dataProtection = services.AddDataProtection()
 				.SetDefaultKeyLifetime(TimeSpan.FromDays(7))
-				.SetApplicationName("VIEApps-NGX")
+				.SetApplicationName(UtilityService.GetAppSetting("DataProtection:Name:Application", "VIEApps-NGX-Portals"))
 				.UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration
 				{
 					EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
 					ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+				})
+				.PersistKeysToDistributedCache(new DistributedXmlRepositoryOptions
+				{
+					Key = UtilityService.GetAppSetting("DataProtection:Key", "DataProtection-Keys"),
+					CacheOptions = new DistributedCacheEntryOptions
+					{
+						AbsoluteExpiration = new DateTimeOffset(DateTime.Now.AddDays(7))
+					}
 				});
+			if ("true".IsEquals(UtilityService.GetAppSetting("DataProtection:DisableAutomaticKeyGeneration")))
+				dataProtection.DisableAutomaticKeyGeneration();
 
 			// MVC
 			services.AddMvc();
 		}
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment environment, IApplicationLifetime appLifetime)
+		public void Configure(IApplicationBuilder appBuilder, IHostingEnvironment environment, IApplicationLifetime appLifetime)
 		{
 			// settings
 			var stopwatch = Stopwatch.StartNew();
-			Global.ServiceName = "Portals";
 			Console.OutputEncoding = Encoding.UTF8;
 
-			var loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
-			var logLevel = this.Configuration.GetAppSetting("Logging/LogLevel/Default", "Information").ToEnum<LogLevel>();
-			var path = UtilityService.GetAppSetting("Path:Logs");
-			if (!string.IsNullOrWhiteSpace(path) && Directory.Exists(path))
+			Global.ServiceName = "Portals";
+			AspNetCoreUtilityService.ServerName = UtilityService.GetAppSetting("ServerName", "VIEApps NGX");
+
+			var loggerFactory = appBuilder.ApplicationServices.GetService<ILoggerFactory>();
+			var logPath = UtilityService.GetAppSetting("Path:Logs");
+			if (!string.IsNullOrWhiteSpace(logPath) && Directory.Exists(logPath))
 			{
-				path = Path.Combine(path, "{Date}" + $"_{Global.ServiceName.ToLower()}.http.all.txt");
-				loggerFactory.AddFile(path, logLevel);
+				logPath = Path.Combine(logPath, "{Date}" + $"_{Global.ServiceName.ToLower()}.http.all.txt");
+				loggerFactory.AddFile(logPath, this.LogLevel);
 			}
 			else
-				path = null;
+				logPath = null;
 
 			Logger.AssignLoggerFactory(loggerFactory);
 			Global.Logger = loggerFactory.CreateLogger<Startup>();
@@ -114,7 +124,7 @@ namespace net.vieapps.Services.Portals
 #endif
 
 			Global.CreateRSA();
-			Global.ServiceProvider = app.ApplicationServices;
+			Global.ServiceProvider = appBuilder.ApplicationServices;
 			Global.RootPath = environment.ContentRootPath;
 
 			JsonConvert.DefaultSettings = () => new JsonSerializerSettings
@@ -128,32 +138,31 @@ namespace net.vieapps.Services.Portals
 			Handler.Connect();
 
 			// middleware
-			app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto });
-			app.UseStatusCodeHandler();
-			app.UseResponseCompression();
-			app.UseCache();
-			app.UseSession();
-			app.UseAuthentication();
-			app.UseMiddleware<Handler>();
-			app.UseMvc(routes => routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
+			appBuilder
+				.UseForwardedHeaders(Global.GetForwardedHeadersOptions())
+				.UseStatusCodeHandler()
+				.UseResponseCompression()
+				.UseCache()
+				.UseSession()
+				.UseAuthentication()
+				.UseMiddleware<Handler>()
+				.UseMvc(routes => routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
 
 			// on started
 			appLifetime.ApplicationStarted.Register(() =>
 			{
-				Global.Logger.LogInformation($"Listening URI: {UtilityService.GetAppSetting("HttpUri:Listen", "http://0.0.0.0:8026")}");
-				Global.Logger.LogInformation($"API Gateway Router: {Router.GetRouterStrInfo()}");
-				Global.Logger.LogInformation($"API Gateway HTTP service URI: {UtilityService.GetAppSetting("HttpUri:APIs")}");
-				Global.Logger.LogInformation($"Files HTTP service URI: {UtilityService.GetAppSetting("HttpUri:Files")}");
-				Global.Logger.LogInformation($"Portals HTTP service URI: {UtilityService.GetAppSetting("HttpUri:Portals")}");
-				Global.Logger.LogInformation($"Root path: {Global.RootPath}");
-				Global.Logger.LogInformation($"Logs path: {UtilityService.GetAppSetting("Path:Logs")}");
-				Global.Logger.LogInformation($"Default logging level: {logLevel} [ASP.NET Core always set logging level by value of appsettings.json]");
-				if (!string.IsNullOrWhiteSpace(path))
-					Global.Logger.LogInformation($"Rolling log files is enabled - Path format: {path}");
-				Global.Logger.LogInformation($"Static files path: {UtilityService.GetAppSetting("Path:StaticFiles")}");
+				Global.Logger.LogInformation($"API Gateway Router: {new Uri(Router.GetRouterStrInfo()).GetResolvedURI()}");
+				Global.Logger.LogInformation($"API Gateway HTTP service: {UtilityService.GetAppSetting("HttpUri:APIs", "None")}");
+				Global.Logger.LogInformation($"Files HTTP service: {UtilityService.GetAppSetting("HttpUri:Files", "None")}");
+				Global.Logger.LogInformation($"Portals HTTP service: {UtilityService.GetAppSetting("HttpUri:Portals", "None")}");
+				Global.Logger.LogInformation($"Passports HTTP service: {UtilityService.GetAppSetting("HttpUri:Passports", "None")}");
+				Global.Logger.LogInformation($"Root (base) directory: {Global.RootPath}");
+				Global.Logger.LogInformation($"Temporary directory: {UtilityService.GetAppSetting("Path:Temp", "None")}");
+				Global.Logger.LogInformation($"Static files directory: {UtilityService.GetAppSetting("Path:StaticFiles", "None")}");
 				Global.Logger.LogInformation($"Static segments: {Global.StaticSegments.ToString(", ")}");
+				Global.Logger.LogInformation($"Logging level: {this.LogLevel} - Rolling log files is {(string.IsNullOrWhiteSpace(logPath) ? "disabled" : $"enabled => {logPath}")}");
 				Global.Logger.LogInformation($"Show debugs: {Global.IsDebugLogEnabled} - Show results: {Global.IsDebugResultsEnabled} - Show stacks: {Global.IsDebugStacksEnabled}");
-				Global.Logger.LogInformation($"Request body limit: {UtilityService.GetAppSetting("Limits:Body", "10")} MB");
+				Global.Logger.LogInformation($"Request limit: {UtilityService.GetAppSetting("Limits:Body", "10")} MB");
 
 				stopwatch.Stop();
 				Global.Logger.LogInformation($"The {Global.ServiceName} HTTP service is started - PID: {Process.GetCurrentProcess().Id} - Execution times: {stopwatch.GetElapsedTimes()}");
