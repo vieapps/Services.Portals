9e985b869a9394c73a5564b3983f9b2d969aa0e0
Quynh Nguyen <mr.quynh@gmail.com>
2020-03-06 22:07:26 +0700

    (refactoring): version info

--- Abstractions/Expression.cs
+++ Abstractions/Expression.cs
@@ -2,12 +2,37 @@
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using net.vieapps.Components.Repository;
 namespace net.vieapps.Services.Portals
 {
 	/// <summary>
-	/// Presents a logging service
+	/// Presents an expression for filtering portal objects
 	/// </summary>
-	public interface IPortalExpression<T> where T : class
+	public interface IPortalExpression : IPortalObject
 	{
+		/// <summary>
+		/// Gets the type name of the content-type definition (means the type name of a repository entity)
+		/// </summary>
+		string DefinitionType { get; }
+
+		/// <summary>
+		/// Gets the identity of a module that this object is belong to
+		/// </summary>
+		string ModuleID { get; }
+
+		/// <summary>
+		/// Gets the identity of a business content-type that the object is belong to
+		/// </summary>
+		string ContentTypeID { get; }
+
+		/// <summary>
+		/// Gets the filter expression
+		/// </summary>
+		IFilterBy FilterBy { get; }
+
+		/// <summary>
+		/// Gets the sort expression
+		/// </summary>
+		ISortBy SortBy { get; }
 	}
 }
