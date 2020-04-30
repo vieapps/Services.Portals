d4f0e6e28a26de8655cc0daba0e631bfc24c0741
Quynh Nguyen <mr.quynh@gmail.com>
2020-05-01 03:02:51 +0700

    (improv): add Extended UI Control Definition

--- Abstractions/Definitions.cs
+++ Abstractions/Definitions.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿#region Related components
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using net.vieapps.Components.Repository;
 using net.vieapps.Components.Utility;
+#endregion
 
 namespace net.vieapps.Services.Portals
 {
@@ -99,12 +102,11 @@ namespace net.vieapps.Services.Portals
 				this.Description = definition.Description;
 				this.Icon = definition.Icon;
 				this.MultipleIntances = definition.MultipleIntances;
-				this.Extendable = definition.Extendable;
 				this.Indexable = definition.Indexable;
-				this.AliasProperty = definition.AliasProperty;
-				this.ParentAssociatedProperty = definition.ParentAssociatedProperty;
-				this.ParentObjectName = definition.ParentType?.GetTypeName(true);
+				this.Extendable = definition.Extendable;
+				this.ExtendedPropertiesBefore = definition.ExtendedPropertiesBefore;
 				this.ObjectName = definition.ObjectName;
+				this.ParentObjectName = definition.ParentType?.GetTypeName(true);
 			}
 		}
 
@@ -134,34 +136,29 @@ namespace net.vieapps.Services.Portals
 		public bool MultipleIntances { get; set; }
 
 		/// <summary>
-		/// Gets or Sets the state that allow to extend the run-time entities by extended properties (means the value of EntityAttribute.Extendable)
-		/// </summary>
-		public bool Extendable { get; set; }
-
-		/// <summary>
 		/// Gets or sets the state that specifies the data of run-time entities are able to index with global search module (means the value of EntityAttribute.Indexable)
 		/// </summary>
 		public bool Indexable { get; set; }
 
 		/// <summary>
-		/// Gets or Sets the name of the property to use as alias (means the value of EntityAttribute.AliasProperty)
+		/// Gets or Sets the state that allow to extend the run-time entities by extended properties (means the value of EntityAttribute.Extendable)
 		/// </summary>
-		public string AliasProperty { get; set; }
+		public bool Extendable { get; set; }
 
 		/// <summary>
-		/// Gets or Sets the name of the property that use to associate with the parent object (means the value of EntityAttribute.ParentAssociatedProperty)
+		/// Gets or Sets the name of the standard property to place extended properties before its (means the value of EntityAttribute.ExtendedPropertiesBefore)
 		/// </summary>
-		public string ParentAssociatedProperty { get; set; }
-
-		/// <summary>
-		/// Gets or Sets the name of the service's object that associates as parent object (means the type-name value of EntityAttribute.ParentType)
-		/// </summary>
-		public string ParentObjectName { get; set; }
+		public string ExtendedPropertiesBefore { get; internal set; }
 
 		/// <summary>
 		/// Gets or sets the name of the service's object that associates with this content-type definition
 		/// </summary>
 		public string ObjectName { get; set; }
+
+		/// <summary>
+		/// Gets or Sets the name of the service's object that associates as parent object (means the type-name value of EntityAttribute.ParentType)
+		/// </summary>
+		public string ParentObjectName { get; set; }
 
 		/// <summary>
 		/// Gets or Sets the type name of the entity definition
@@ -179,5 +176,167 @@ namespace net.vieapps.Services.Portals
 		/// </summary>
 		[JsonIgnore, XmlIgnore]
 		public ModuleDefinition ModuleDefinition { get; set; }
+	}
+
+	//  ------------------------------------------------------------------------
+
+	/// <summary>
+	/// Presents an UI definition for working with an extended property of a repository entity in a respository 
+	/// </summary>
+	public sealed class ExtendedUIControlDefinition
+	{
+		public ExtendedUIControlDefinition(JObject json = null)
+			=> this.CopyFrom(json ?? new JObject());
+
+		public override string ToString()
+			=> this.ToJson().ToString(Formatting.None);
+
+		#region Properties
+		/// <summary>
+		/// Gets or sets the name
+		/// </summary>
+		public string Name { get; set; } = "";
+
+		/// <summary>
+		/// Gets or sets the excluded state
+		/// </summary>
+		public bool Excluded { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the hidden state
+		/// </summary>
+		public bool Hidden { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the state that mark this property is hidden in the view or not
+		/// </summary>
+		public bool? HiddenInView { get; set; }
+
+		/// <summary>
+		/// Gets or sets the require state
+		/// </summary>
+		public bool Required { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the label - use doube braces to specified code of a language resource - ex: {{common.buttons.ok}}
+		/// </summary>
+		public string Label { get; set; }
+
+		/// <summary>
+		/// Gets or sets the place-holder - use doube braces to specified code of a language resource - ex: {{common.buttons.ok}}
+		/// </summary>
+		public string PlaceHolder { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description - use doube braces to specified code of a language resource - ex: {{common.buttons.ok}}
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the RegEx pattern for data validation
+		/// </summary>
+		public bool ValidatePattern { get; set; }
+
+		/// <summary>
+		/// Gets or sets the order number
+		/// </summary>
+		public int? Order { get; set; }
+
+		/// <summary>
+		/// Gets or sets the disable state
+		/// </summary>
+		public bool? Disabled { get; set; }
+
+		/// <summary>
+		/// Gets or sets the read-only state
+		/// </summary>
+		public bool? ReadOnly { get; set; }
+
+		/// <summary>
+		/// Gets or sets the auto-focus state
+		/// </summary>
+		public bool? AutoFocus { get; set; }
+
+		/// <summary>
+		/// Gets or sets the min value
+		/// </summary>
+		public double? MinValue { get; set; }
+
+		/// <summary>
+		/// Gets or sets the max value
+		/// </summary>
+		public double? MaxValue { get; set; }
+
+		/// <summary>
+		/// Gets or sets the min-length
+		/// </summary>
+		public int? MinLength { get; set; }
+
+		/// <summary>
+		/// Gets or sets the max-length
+		/// </summary>
+		public int? MaxLength { get; set; }
+
+		/// <summary>
+		/// Gets or sets the width
+		/// </summary>
+		public string Width { get; set; }
+
+		/// <summary>
+		/// Gets or sets the height
+		/// </summary>
+		public string Height { get; set; }
+
+		/// <summary>
+		/// Gets or sets the state to act as text/html editor
+		/// </summary>
+		public bool? AsTextEditor { get; set; }
+
+		/// <summary>
+		/// Gets or sets the date-picker with times
+		/// </summary>
+		public bool? DatePickerWithTimes { get; set; }
+
+		/// <summary>
+		/// Gets or sets the multiple of select/lookup control
+		/// </summary>
+		public bool? Multiple { get; set; }
+
+		/// <summary>
+		/// Gets or sets the values of select control (JSON string)
+		/// </summary>
+		public string SelectValues { get; set; }
+
+		/// <summary>
+		/// Gets or sets the 'as-boxes' of select control
+		/// </summary>
+		public bool? SelectAsBoxes { get; set; }
+
+		/// <summary>
+		/// Gets or sets the interface mode of select control (alert, popover, actionsheet)
+		/// </summary>
+		public string SelectInterface { get; set; }
+
+		/// <summary>
+		/// Gets or sets the mode for looking-up (Address, User or Business Object)
+		/// </summary>
+		public string LookupMode { get; set; }
+
+		/// <summary>
+		/// Gets or sets the identity of the business repository for looking-up
+		/// </summary>
+		public string LookupRepositoryID { get; set; }
+
+		/// <summary>
+		/// Gets or sets the identity of the business entity for looking-up
+		/// </summary>
+		public string LookupRepositoryEntityID { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of business entity's property for displaying while looking-up
+		/// </summary>
+		public string LookupProperty { get; set; }
+		#endregion
+
 	}
 }
