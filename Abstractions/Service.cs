019712909e8b53c0af1968b29d0a2313c32cf454
Quynh Nguyen <mr.quynh@gmail.com>
2021-02-04 09:17:57 +0700

    (improv): only support .NET 5.0 and .NET Standard 2.0

--- Abstractions/Service.cs
+++ Abstractions/Service.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using WampSharp.V2.Rpc;
 using Newtonsoft.Json.Linq;
-using net.vieapps.Components.Repository;
-using net.vieapps.Components.Security;
 
 namespace net.vieapps.Services.Portals
 {
@@ -14,24 +10,6 @@ namespace net.vieapps.Services.Portals
 	/// </summary>
 	public interface ICmsPortalsService : IService, IUniqueService, IServiceComponent
 	{
-		/// <summary>
-		/// Registers the service with API Gateway
-		/// </summary>
-		/// <param name="args">The arguments for registering</param>
-		/// <param name="onSuccess">The action to run when the service was registered successful</param>
-		/// <param name="onError">The action to run when got any error</param>
-		/// <returns></returns>
-		Task RegisterServiceAsync(IEnumerable<string> args, Action<IService> onSuccess = null, Action<Exception> onError = null);
-
-		/// <summary>
-		/// Unregisters the service with API Gateway
-		/// </summary>
-		/// <param name="args">The arguments for unregistering</param>
-		/// <param name="available">true to mark the service still available</param>
-		/// <param name="onSuccess">The action to run when the service was unregistered successful</param>
-		/// <param name="onError">The action to run when got any error</param>
-		Task UnregisterServiceAsync(IEnumerable<string> args, bool available = true, Action<IService> onSuccess = null, Action<Exception> onError = null);
-
 		/// <summary>
 		/// Gets the definition for working with CMS Portals
 		/// </summary>
